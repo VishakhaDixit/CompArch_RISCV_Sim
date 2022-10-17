9dce4b4ffb9962785c4fc2ba6280bedb9d9b61eb
VishakhaDixit <vishakhadixit1996@gmail.com>
2022-10-17 08:33:36 -0400

    Adding dport class for Data port.

---
+++ dport.h
@@ -0,0 +1,28 @@
+/**************************
+ *
+ * @file    dram.h
+ *
+ * @brief   This file contains dram class for DRAM
+ *
+ * @date    Oct 14, 2022
+ *
+ * @author  Vishakha Dixit
+ *
+ **************************/
+
+#ifndef __DPORT_H__
+#define __DPORT_H__
+
+#include <stdint.h>
+
+class dport
+{
+    private:
+        uint32_t startAddr;
+        uint32_t endAddr;
+    public:
+        uint8_t getData(uint32_t addr);
+        void    setData(uint32_t addr, uint8_t data);
+};
+
+#endif
