1efdbc815b72931779d80aa3853d5f9c241d3fb9
VishakhaDixit <vishakhadixit1996@gmail.com>
2022-10-18 08:43:39 -0400

    Make dport friend of dram for memory access. Include dram pointer.

--- dport.h
+++ dport.h
@@ -14,13 +14,21 @@
 #define __DPORT_H__
 
 #include <stdint.h>
+#include "dram.h"
 
 class dport
 {
     private:
         uint32_t startAddr;
         uint32_t endAddr;
+        dram *p_dram;
     public:
+        dport(dram* p_d, uint32_t dportStart, uint32_t dportEnd)
+        {
+            startAddr = dportStart;
+            endAddr = dportEnd;
+            p_dram = p_d;
+        };
         uint32_t getData(uint32_t addr);
         void    setData(uint32_t addr, uint32_t data);
 };
