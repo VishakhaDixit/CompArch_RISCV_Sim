ee8b62fdd6f6a460175be70db676434ce88e6471
VishakhaDixit <vishakhadixit1996@gmail.com>
2022-10-19 09:13:14 -0400

    Implement initDram and Print Dram functions

--- dram.cpp
+++ dram.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "dram.h"
 #include "iport.h"
 #include "dport.h"
@@ -18,12 +19,24 @@ dport *dram::getDataPort()
     return dp;
 }
 
-dram::initDram(uint32_t startAddr, std::array<uint8_t, 0x1400> initData)
+void dram::initDram(uint32_t startAddr, std::array<uint8_t, 0x1400> initData)
 {
-
+    std::copy(std::begin(initData), std::end(initData), std::begin(memory));
 }
 
-dram::printDram()
+void dram::printDram()
 {
-
+    std::cout << "==============DRAM Dump==============" << "\n\n";
+    std::cout << std::to_string(memory[0]) << "  ";
+    std::cout << std::to_string(memory[1]) << "  ";
+    std::cout << std::to_string(memory[2]) << "  ";
+    std::cout << std::to_string(memory[3]) << "  ";
+    for (uint32_t i = 4; i < 0x1400; i++)
+    {
+        if ((i % 4 == 0) && (i  != 0))
+        {
+            std::cout<< "\n";
+        }
+        std::cout << std::to_string(memory[i]) << "  ";
+    }
 }
