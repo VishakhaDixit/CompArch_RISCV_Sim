540112babd72d4c0ccfb54ebbda2241e59ab5f6e
VishakhaDixit <vishakhadixit1996@gmail.com>
2022-10-20 08:14:33 -0400

    Corrected logic for DRAM set and get data port.

--- dport.cpp
+++ dport.cpp
@@ -15,7 +15,7 @@
 uint32_t dport::getData(uint32_t addr)
 {
     uint32_t data;
-    if ((addr > startAddr) && (addr < endAddr))
+    if ((addr >= startAddr) && (addr < endAddr))
     {
          data = (p_dram -> memory[addr] << 24)     | 
                 (p_dram -> memory[addr + 1] << 16) | 
@@ -33,10 +33,10 @@ void dport::setData(uint32_t addr, uint32_t data)
 {
     if ((addr >= startAddr) && (addr < endAddr))
     {
-        p_dram -> memory[addr]     = data & 0xFF000000;
-        p_dram -> memory[addr + 1] = data & 0x00FF0000;
-        p_dram -> memory[addr + 2] = data & 0x0000FF00;
-        p_dram -> memory[addr + 3] = data & 0x000000FF;
+        p_dram -> memory[addr]     = (uint8_t)((data & 0xFF000000) >> 24);
+        p_dram -> memory[addr + 1] = (uint8_t)((data & 0x00FF0000) >> 16);
+        p_dram -> memory[addr + 2] = (uint8_t)((data & 0x0000FF00) >> 8);
+        p_dram -> memory[addr + 3] = (uint8_t)(data & 0x000000FF);
     }
     else
     {
