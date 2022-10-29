790980f3f7c4909ebae8484b61e03d46ab049729
VishakhaDixit <vishakhadixit1996@gmail.com>
2022-10-29 14:59:29 -0400

    Initialize simulator for CPU1 testing.

--- main.cpp
+++ main.cpp
@@ -291,8 +291,8 @@ int main() {
 	Simulator *cpu0 = new Simulator(sys, arb, 0);
 	cpu0->initSim();				//Initialize Simulator device.
 		
-	// Simulator *cpu1 = new Simulator(sys, arb, 1);
-	// cpu1->initSim();				//Initialize Simulator device.
+	Simulator *cpu1 = new Simulator(sys, arb, 1);
+	cpu1->initSim();				//Initialize Simulator device.
 
 	sys->executeSim(200000);		//Run Simulator for 100 clk cycles, this function terminates if pipeline is flushed.
 	ram.printDram(0xC00, 0xFFF);
